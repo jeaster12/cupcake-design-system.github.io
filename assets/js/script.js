0d706daac134b4a583c612c253b27239bf724205
Josh Easter <>
2019-03-17 20:58:34 -0400

    cleanups

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,11 @@ $(document).ready(function($) {
 
     $(selectTab).fadeIn();
   });
-});
 
-
-$(function () {
 
 /************************************************
 * SmoothScroll
 *************************************************/
-
 var scroll = new SmoothScroll('a[href*="#"]', {
   // Selectors
   ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
@@ -35,57 +31,35 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 });
 
 
-
 /************************************************
 * Disable empty links in docs examples
-*************************************************/    
-
-$('.docs-content [href="#"]').click(function (e) {
+*************************************************/
+$('.main-demo-content [href="#"]').click(function (e) {
   e.preventDefault()
 })
  
-
 $('[href=""]').click(function (e) {
   e.preventDefault()
 })
  
 
-
 /************************************************
 * Docs Highlight
 *************************************************/  
-
 $('html').on('click.ui.btn', '#highlight-toggle', function(e) {
   e.preventDefault();
   $(this).find(".fa-caret-down, .fa-caret-up").toggleClass("fa-caret-down fa-caret-up");
-  // $(this).parents(".window-bar").nextAll(".highlight:first").toggleClass("show-me");
-  // $(this).prev().slideToggle(200);
   $(this).prev().toggleClass("show-me");
 });
 
 
-// $('div.docs-example').each(function () {
-//   var bar = '<div class="window-bar"><div class="circles"><span class="circle circle-red"></span><span class="circle circle-yellow"></span><span class="circle circle-green"></span></div><div class="languages"><span class="c-btn c-btn-secondary c-btn-xs" id="highlight-toggle">Show Html</span></div</div>'
-//   $(this).before(bar)
-// })
-
-// $('div.docs-example').each(function () {
-//   var bar = '<div class="window-bar"><div class="languages"><span class="c-btn c-btn-secondary c-btn-xs" id="highlight-toggle">Show Html</span></div></div>'
-//   $(this).after(bar)
-// })
-
-
-
 /************************************************
 * Clipboard
-*************************************************/  
-
+*************************************************/
 $('div.highlight pre').each(function () {
   var btnHtml = '<div class="docs-clipboard"><button class="docs-btn c-btn c-btn-xs c-btn-primary" data-tooltip="Copy To Clipboard" data-tooltip-conf="right square">Copy</button></div>'
   $(this).after(btnHtml)
 })
-
-
 
 var clipboard = new Clipboard('.docs-btn', {
   target: function (trigger) {
@@ -110,18 +84,14 @@ clipboard.on('error', function (e) {
 })
 
 
-
 /************************************************
 * Add deep anchors
 *************************************************/  
 anchors.options = {
   icon: '#'
 }
-anchors.add('.docs-content > h2, .docs-content > h3, .docs-content > h4, .docs-content > h5')
-$('.docs-content > h2, .docs-content > h3, .docs-content > h4, .docs-content > h5').wrapInner('<div></div>')
-
-
-
+anchors.add('.main-demo-content > h2, .main-demo-content > h3, .main-demo-content > h4, .main-demo-content > h5')
+$('.main-demo-content > h2, .main-demo-content > h3, .main-demo-content > h4, .main-demo-content > h5').wrapInner('<div></div>')
 
 
 /************************************************
@@ -134,7 +104,6 @@ clipboard.on('success', function (e) {
       .attr('tooltip', 'Copied!')
     e.clearSelection()
   })
-
 
   $('#docs-menu-toggle').on('click', function(e) {
     e.preventDefault();
